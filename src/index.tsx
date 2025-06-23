939820f1fe2ee0cf3be81c2b3de0c2a96b64285a
unknown <vladhafarov@gmail.com>
2025-06-23 12:02:02 +0300

    solution

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,24 @@
-import { createRoot } from 'react-dom/client';
-import { HashRouter as Router } from 'react-router-dom';
+// import { createRoot } from 'react-dom/client';
+// import { HashRouter as Router } from 'react-router-dom';
 
+// import 'bulma/css/bulma.css';
+// import '@fortawesome/fontawesome-free/css/all.css';
+
+// import { App } from './App';
+// import React from 'react';
+
+// createRoot(document.getElementById('root') as HTMLDivElement).render(
+//   <Router>
+//     <App />
+//   </Router>,
+// );
+
+import { createRoot } from 'react-dom/client';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
+import { Root } from './Root';
+import React from 'react';
 
-import { App } from './App';
+const container = document.getElementById('root') as HTMLElement;
 
-createRoot(document.getElementById('root') as HTMLDivElement).render(
-  <Router>
-    <App />
-  </Router>,
-);
+createRoot(container).render(<Root />);
